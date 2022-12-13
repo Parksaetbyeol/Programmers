c3efdac5d01eb4a805b0ad8649024d4f109b7382
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2022-12-14 02:05:17 +0900

    [level 3] Title: 오랜 기간 보호한 동물(2), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
+++ 프로그래머스/lv3/59411. 오랜 기간 보호한 동물（2）/오랜 기간 보호한 동물（2）.sql
@@ -1,9 +1,11 @@
 -- 코드를 입력하세요
-# SELECT A.datetime - B.datetime
+-- 보호 기간이 긴 데이터 조회
+# SELECT B.datetime - A.datetime
 # FROM animal_ins A JOIN animal_outs B ON A.animal_id = B.animal_id
-# ORDER BY (A.datetime - B.datetime) DESC
+# ORDER BY (B.datetime - A.datetime) DESC
 
+-- 정답 코드
 SELECT A.animal_id, A.name
 FROM animal_ins A INNER JOIN animal_outs B USING(animal_id)
-ORDER BY (B.datetime - A.datetime) DESC
-LIMIT 2
+ORDER BY (B.datetime - A.datetime) DESC # DATEDIFF(B.datetime, A.datetime)
+LIMIT 2 -- 두마리만
