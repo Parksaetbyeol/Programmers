cd775b500ff015aca942db4cb28f5294b9a9a26b
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2022-12-16 02:35:13 +0900

    [level 3] Title: 헤비 유저가 소유한 장소, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
+++ 프로그래머스/lv3/77487. 헤비 유저가 소유한 장소/헤비 유저가 소유한 장소.sql
@@ -1,9 +1,4 @@
 -- 코드를 입력하세요
-# WITH host_cnt AS (SELECT COUNT(host_id) cnt
-#                  FROM places
-#                  GROUP BY host_id
-#                  HAVING COUNT(host_id) > 1)
-
 SELECT id, name, host_id
 FROM PLACES
 WHERE host_id IN (SELECT host_id FROM places 
