ded392bca2e2931bd5333194f2e90a595bcc14f9
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2022-12-22 23:58:01 +0900

    [level 4] Title: 보호소에서 중성화한 동물, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
+++ 프로그래머스/lv4/59045. 보호소에서 중성화한 동물/보호소에서 중성화한 동물.sql
@@ -4,3 +4,7 @@ FROM animal_ins A INNER JOIN animal_outs B ON A.animal_id = B.animal_id
 WHERE A.sex_upon_intake LIKE 'Intact%'
     AND B.sex_upon_outcome NOT LIKE 'Intact%'
 ORDER BY A.animal_id
+
+# SELECT O.ANIMAL_ID, O.ANIMAL_TYPE, O.NAME
+# FROM ANIMAL_OUTS O JOIN ANIMAL_INS I ON I.ANIMAL_ID = O.ANIMAL_ID
+# WHERE I.SEX_UPON_INTAKE <> O.SEX_UPON_OUTCOME
