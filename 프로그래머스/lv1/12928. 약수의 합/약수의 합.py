594d71d3cfa52a730605f23ccaeb22c530fe5e71
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-01-03 13:46:38 +0900

    [level 1] Title: 약수의 합, Time: 0.27 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -1,7 +1,11 @@
+# 1
 def solution(n):
     answer = 0
     for i in range(1, n+1):
         if n % i == 0:
             answer += i
     return answer
-        
+
+# 2
+# def solution(n):
+#     return sum([i for i in range(1,n+1) if n % i == 0])
