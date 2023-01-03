fb5f367bb8c107479648fd534b5b71c9c96ad29f
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-01-03 14:10:16 +0900

    [level 1] Title: 자연수 뒤집어 배열로 만들기, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
+++ 프로그래머스/lv1/12932. 자연수 뒤집어 배열로 만들기/자연수 뒤집어 배열로 만들기.py
@@ -1,5 +1,14 @@
+# 1
+# def solution(n):
+#     answer = []
+#     for i in reversed(str(n)):
+#         answer.append(int(i)) # 인덱스 하나하나에 접근해 int로 변경해서 저장
+#     return answer
+
+# 2
+# def solution(n):
+#     return [int(i) for i in reversed(str(n))]
+
+# 3 map 사용
 def solution(n):
-    answer = []
-    for i in reversed(str(n)):
-        answer.append(int(i))
-    return answer
+    return list(map(int, reversed(str(n)))) # reversed한 n을 전부 int로 변환
