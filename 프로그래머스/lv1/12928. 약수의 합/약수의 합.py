8ef6cea8578e72735377b400f6f27a6670e85d67
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-01-11 20:51:10 +0900

    Update 약수의 합.py

--- 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
+++ 프로그래머스/lv1/12928. 약수의 합/약수의 합.py
@@ -7,5 +7,5 @@ def solution(n):
     return answer
 
 # 2
-# def solution(n):
-#     return sum([i for i in range(1,n+1) if n % i == 0])
+def solution(n):
+    return sum([i for i in range(1,n+1) if n % i == 0])
