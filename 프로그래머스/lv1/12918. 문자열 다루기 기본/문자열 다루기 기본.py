63b576713a48841f4fd1253f1c730f2c9b24964d
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-02-02 22:36:31 +0900

    Update 문자열 다루기 기본.py

--- 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
+++ 프로그래머스/lv1/12918. 문자열 다루기 기본/문자열 다루기 기본.py
@@ -1,8 +1,8 @@
-# def solution(s):
-#     if (len(s) == 4 or len(s) == 6) and s.isdigit():
-#         return True
-#     else:
-#         return False
+def solution(s):
+    if (len(s) == 4 or len(s) == 6) and s.isdigit():
+        return True
+    else:
+        return False
   
 # 2
 def solution(s):
