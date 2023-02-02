bafd54568af7a54153b3363b0008eed42d277b0d
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-02-02 23:06:41 +0900

    Update 행렬의 덧셈.py

--- 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
+++ 프로그래머스/lv1/12950. 행렬의 덧셈/행렬의 덧셈.py
@@ -1,12 +1,14 @@
-# import numpy as np
+# 1 - numpy 
+import numpy as np
 
-# def solution(arr1, arr2):
-#     np_arr1 = np.array(arr1)
-#     np_arr2 = np.array(arr2)
-#     sum_arr = np_arr1 + np_arr2
+def solution(arr1, arr2):
+    np_arr1 = np.array(arr1)
+    np_arr2 = np.array(arr2)
+    sum_arr = np_arr1 + np_arr2
     
-#     return sum_arr.tolist()
+    return sum_arr.tolist()
 
+# 2
 def solution(arr1, arr2):
     for i in range(len(arr1)):
         for j in range(len(arr1[0])):
