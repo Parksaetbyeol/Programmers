e72d5d281521e5b97384702d0111c9cb38b769ae
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-02-04 23:30:22 +0900

    Update 직사각형 별찍기.py

--- 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.py
+++ 프로그래머스/lv1/12969. 직사각형 별찍기/직사각형 별찍기.py
@@ -6,6 +6,6 @@ for i in range(b): # b = 3, *을 5번 print한거를 3번 반복
     print('')
     
 # 2 - for문없이 해결
-# a, b = map(int, input().strip().split(' '))
-# answer = ('*'*a + '\n') * b
-# print(answer)
+a, b = map(int, input().strip().split(' '))
+answer = ('*'*a + '\n') * b
+print(answer)
