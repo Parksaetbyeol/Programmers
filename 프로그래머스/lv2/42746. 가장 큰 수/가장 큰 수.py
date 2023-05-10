675a4976896fc76d7a9e19129196cc4f4703d1c8
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-05-10 23:32:03 +0900

    [level 2] Title: 가장 큰 수, Time: 0.01 ms, Memory: 7.89 MB -BaekjoonHub

--- 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/lv2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,4 +1,4 @@
 def solution(numbers):
-    numbers = list(map(str, numbers))
-    numbers.sort(key=lambda x: x * 3, reverse=True)
-    return str(int(''.join(numbers)))
+    nums = list(map(str, numbers)) # numbers를 하나씩 str로 변경해서 list로 묶기
+    nums.sort(key = lambda x : x * 3, reverse = True) #number는 1000이하 이니까 x3(반복)한 값으로 비교해서 역정렬
+    return str(int(''.join(nums)))
