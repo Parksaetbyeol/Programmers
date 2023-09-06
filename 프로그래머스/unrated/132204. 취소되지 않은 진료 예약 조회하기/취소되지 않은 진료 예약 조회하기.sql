d0008d0b38b4c4005c19a963e07215151ca7669a
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-09-06 21:52:51 +0900

    Delete 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기 directory

--- 프로그래머스/unrated/132204. 취소되지 않은 진료 예약 조회하기/취소되지 않은 진료 예약 조회하기.sql
+++
@@ -1,7 +0,0 @@
-SELECT APNT_NO, PT_NAME, AP.PT_NO, D.MCDP_CD, D.DR_NAME, AP.APNT_YMD
-FROM appointment AP JOIN patient P ON AP.pt_no = P.pt_no
-    JOIN doctor D ON AP.mddr_id = D.dr_id
-WHERE apnt_ymd LIKE '2022-04-13%' -- 혹은 DATE_FORMAT(apnt_ymd,'%Y-%m-%d') = '2022-04-13'
-    AND apnt_cncl_yn = 'N' -- 예약취소여부
-    AND D.MCDP_CD = 'CS' -- 흉부외과 (cs)
-ORDER BY 6;
