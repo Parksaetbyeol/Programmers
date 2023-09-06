5f762a93e56479f3ade23dc3cb072fad77970968
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-09-06 21:45:16 +0900

    Delete 프로그래머스_sql/lv4/59413. 입양 시각 구하기（2） directory

--- 프로그래머스_sql/lv4/59413. 입양 시각 구하기（2）/입양 시각 구하기（2）.sql
+++
@@ -1,9 +0,0 @@
--- 코드를 입력하세요
-SET @HOUR := -1;  # HOUR 변수에 -1 넣기
-
-SELECT (@HOUR := @HOUR +1) AS HOUR,  # @HOUR에 +1 씩 대입하기
-    (SELECT COUNT(*) 
-     FROM animal_outs 
-     WHERE HOUR(datetime) = @HOUR) AS COUNT 
-FROM animal_outs
-WHERE @HOUR < 23  # 23시까지니까 22+1에서 종료.
