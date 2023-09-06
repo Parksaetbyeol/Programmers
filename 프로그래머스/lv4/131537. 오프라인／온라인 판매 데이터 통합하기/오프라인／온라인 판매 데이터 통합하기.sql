c41448985761e9f9e5606eef8ad3684a24f3508d
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2023-09-06 21:51:02 +0900

    Delete 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기 directory

--- 프로그래머스/lv4/131537. 오프라인／온라인 판매 데이터 통합하기/오프라인／온라인 판매 데이터 통합하기.sql
+++
@@ -1,6 +0,0 @@
-SELECT DATE_FORMAT(sales_date,'%Y-%m-%d') SALES_DATE, product_id, user_id, sales_amount
-FROM (SELECT * FROM online_sale
-     UNION ALL
-        SELECT offline_sale_id, NULL AS USER_ID, product_id, sales_amount, sales_date FROM offline_sale) A -- 칼럼 수 맞춰야함
-WHERE A.sales_date LIKE '2022-03%'
-ORDER BY SALES_DATE, PRODUCT_ID, USER_ID
