e0f5c5a427142f60e2a92a9f43b298bd13e93a0a
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2024-01-30 14:50:46 +0900

    Delete 프로그래머스/unrated directory

--- 프로그래머스/unrated/164668. 조건에 맞는 사용자와 총 거래금액 조회하기/조건에 맞는 사용자와 총 거래금액 조회하기.sql
+++
@@ -1,9 +0,0 @@
-SELECT user_id, nickname, TOTAL_SALES
-FROM USED_GOODS_USER U
-    JOIN (SELECT writer_id, SUM(price) AS TOTAL_SALES
-            FROM USED_GOODS_BOARD
-            GROUP BY writer_id, status
-            HAVING status  = 'DONE'
-                AND SUM(price) >= 700000) B
-    ON B.WRITER_ID = U.USER_ID
-ORDER BY 3;
