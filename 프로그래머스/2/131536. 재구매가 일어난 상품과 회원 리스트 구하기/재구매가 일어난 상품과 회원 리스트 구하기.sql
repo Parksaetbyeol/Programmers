c51551186c3fadfaa2e91f13db62a742c7cd10eb
saetbyeol <107995643+Parksaetbyeol@users.noreply.github.com>
2024-01-30 14:49:19 +0900

    Delete 프로그래머스/2 directory

--- 프로그래머스/2/131536. 재구매가 일어난 상품과 회원 리스트 구하기/재구매가 일어난 상품과 회원 리스트 구하기.sql
+++
@@ -1,5 +0,0 @@
-SELECT user_id, product_id
-FROM ONLINE_SALE
-GROUP BY user_id, product_id
-HAVING COUNT(*) > 1
-ORDER BY USER_ID, PRODUCT_ID DESC
--- 프로그래머스/2/157342. 자동차 평균 대여 기간 구하기/자동차 평균 대여 기간 구하기.sql
+++
@@ -1,6 +0,0 @@
-SELECT CAR_ID, 
-    ROUND(SUM(DATEDIFF(end_date, start_date)+1)/COUNT(car_id), 1) AS AVERAGE_DURATION
-FROM CAR_RENTAL_COMPANY_RENTAL_HISTORY
-GROUP BY car_id
-HAVING ROUND(SUM(DATEDIFF(end_date,start_date)+1)/count(car_id),1)>=7 #평균 대여기간이 7일 이상
-ORDER BY 2 DESC,1 DESC
